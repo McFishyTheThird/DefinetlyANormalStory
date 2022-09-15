a677f9f6f96ca81eae2f93379b294340e7a4d1bc
William <william.lignell@elev.ga.ntig.se>
2022-09-15 14:40:12 +0200

    mjyfjyf

---
+++ DefinetlyANormalStory/DefinetlyANormalStory/Program.cs
@@ -0,0 +1,194 @@
+﻿
+Console.WriteLine("Hello traveler. What is your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}.");
+Console.ReadLine();
+Console.WriteLine("Press enter to continue reading.");
+Console.WriteLine("Write A or B to make you choice(With big letters).");
+
+if (name == "King fishy the third of the fish kingdom fishlandia")
+{
+    Console.WriteLine("You should not be here your majesty. There could be assasins lurking around here.");
+    Console.ReadLine();
+    Console.WriteLine("A masked man steps out from behind a rock and shoots you with his gun.");
+    Console.ReadLine();
+    Console.WriteLine("Fishy Ending (0/7)");
+}
+else if (name == "Cringe Lord")
+{
+    Console.WriteLine("The system can't handle that much cringe and automaticly deletes you.");
+    Console.ReadLine();
+    Console.WriteLine("Why would you choose that name?");
+}
+else
+{
+    Console.WriteLine("You are in an unkown area...although you don't know that since you are sleeping. Do you wake up? A:Yes B:No");
+    string answer = Console.ReadLine();
+    if (answer == "B")
+    {
+        Console.WriteLine("Since you are still sleeping you are easy prey for predators and get eaten by something...Probably a duck...or a squirrel...or maybe even a mix...imagine getting eaten by a ducksquirrel.");
+        Console.ReadLine();
+        Console.WriteLine("You probably feel really stupid for not waking up right now.");
+        Console.ReadLine();
+        Console.WriteLine("Sleepy Ending (1/7)");
+    }
+    else if (answer == "A")
+    {
+        Console.WriteLine("You wake up inside a basement that you suspect belongs to the infamous villain Old lady person");
+        Console.WriteLine("also known as some random person who kidnapped you and that you suspect is some old lady because of no reason what so ever.");
+        Console.ReadLine();
+        Console.WriteLine("You wonder how you could possibly escape from the awful pie baking monster.");
+        Console.WriteLine("You should just give up and wonder about your life choices...");
+        Console.WriteLine("OR you could use a lockpick you randomly found to break open the door or you could just wait for the perfect moment.");
+        Console.ReadLine();
+        Console.WriteLine("The choice is yours. A:lockpick B:wait");
+        string answer2 = Console.ReadLine();
+        if (answer2 == "B")
+        {
+            Console.WriteLine("You choose to wait for the perfect moment.");
+            Console.ReadLine();
+            Console.WriteLine("After around 65 years of waiting nothing has happened and you are now 80 years old and crazy and somehow not dead because of starvation.");
+            Console.WriteLine("You did find something interesting though. During all these years you found a crystal of ancient gods bla bla bla.");
+            Console.WriteLine("Do you use the crystal?A:Eat it B:Yes");
+            string answer3 = Console.ReadLine();
+            if (answer3 == "A")
+            {
+                Console.WriteLine("For some reason you put the crystal in you mouth.");
+                Console.ReadLine();
+                Console.WriteLine("A few minutes go by but the you feel the power going through you veins. You no longer have control over your body");
+                Console.WriteLine("and you end up going on a rampage with these new powers although you don't mind since if theres no one else you just get free icecream");
+                Console.ReadLine();
+                Console.WriteLine("Free ice cream ending (2/7)");
+            }
+            else if (answer3 == "B")
+            {
+                Console.WriteLine("You use the crystal and feel godlike power going trough your veins.");
+                Console.ReadLine();
+                Console.WriteLine("The house explodes and you can hear the doom music kick in. You see Old lady person and it seems like she also found a similar crystal.");
+                Console.ReadLine();
+                Console.WriteLine("Why are you doing this? You ask. Old lady person doesn't answer but to your suprise she takes her face of which actually was a mask and Old lady person is actually a super sexy man also known as Ryan Reynolds.");
+                Console.ReadLine();
+                Console.WriteLine("How will you defeat Ryan Reynolds?A:Wait B:Attack");
+                string answer4 = Console.ReadLine();
+                if (answer4 == "A")
+                {
+                    Console.WriteLine("Ryan Reynolds charges up his super sexy god powers and throws the first punch. You barely dodge his attack but he turns around a uses his super sexy laser eyes to turn you to ashes");
+                    Console.ReadLine();
+                    Console.WriteLine("Sexy eyes ending (3/7)");
+                }
+                else if (answer4 == "B")
+                {
+                    Console.WriteLine("You charge up your averege god powers and lounge forward. You try to punch him but suddenly stop.");
+                    Console.ReadLine();
+                    Console.WriteLine("You realise he's to sexy and by hitting him you will destroy his sexy body thus making you unable to do anything against him.");
+                    Console.ReadLine();
+                    Console.WriteLine("Ryan Reynold uses his super sexy god powers to weaken you. You vision is turning black and the last thing you see is Ryan Reynolds super sexily towering over you and then you die because your body can't handle his super sexyness.");
+                    Console.ReadLine();
+                    Console.WriteLine("Super sexy Ryan Reynold anime battle ending (4/7)");
+
+                }
+                else
+                {
+                    Console.WriteLine("Ryan Reynolds uses his super sexy super beam om super sexyness and obliterates you before you have a chance to react.");
+                    Console.ReadLine();
+                    Console.WriteLine("S.S.B.O.S.S(FAKE Ending(5/6))");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The crystal explodes."); 
+                Console.ReadLine();
+                Console.WriteLine("You somehow survive but the explosion has attracted the attention of very hostile Old lady aliens");
+                Console.ReadLine();
+                Console.WriteLine("A few hours go by and you are now the last human left. The rest has been turned into pies.");
+                Console.ReadLine();
+                Console.WriteLine("Old lady invasion (FAKE Ending(3/6))");
+            }
+        }
+        else if (answer2 == "A")
+        {
+            Console.WriteLine("You choose to use the lockpick that you found but before you get the chance to use it you realise its actually an inflatable chainsaw.");
+            Console.ReadLine();
+            Console.WriteLine("You use the inflatable chainsaw to blow up the door since it was a bomb too.");
+            Console.ReadLine();
+            Console.WriteLine("You know theres only one thing left to do and that is finding Old lady person and destroy this awful villian once and for all.");
+            Console.ReadLine();
+            Console.WriteLine($"You realise that you happen to be in a resturant and is shocked to see that its the famous Kentucky Fried {name}s");
+            Console.WriteLine("What do you do with this knowledge?A:Use thunderbolt B:Search");
+            string Thunderbolt = Console.ReadLine();
+            if (Thunderbolt == "A")
+            {
+                Console.WriteLine("For some reason you try to use thunderbolt but since you aren't pikachu you try to cut some electrical wires using your fingers and end up electricuting yourself");
+                Console.ReadLine();
+                Console.WriteLine("Pikachu ending (5/7)");
+            }
+            else if (Thunderbolt == "B")
+            {
+                Console.WriteLine("You start searching around in the area and accidentaly bump into an old lady. She turns around and you instantly recognise her.");
+                Console.ReadLine();
+                Console.WriteLine("There's no doubt about it. She is definetly maybe Old lady person.");
+                Console.ReadLine();
+                Console.WriteLine("What do you do?A:Attack her B:RUN!");
+                string oldlady = Console.ReadLine();
+                if (oldlady == "A")
+                {
+                    Console.WriteLine("You lounge forward ready to strike but Old lady person turns out to be ripped and has no trouble stopping you.");
+                    Console.ReadLine();
+                    Console.WriteLine("She pulls of her mask to reveal that she is actually some random person who kidnapped you and that you suspect is some old lady because of no reason what so ever ");
+                    Console.WriteLine("also known as Old lady person");
+                    Console.ReadLine();
+                    Console.WriteLine($"You gasp in suprise. How could this be? If only you had known. She putts you in the fryer and before you know it you have been turned into delicious fried {name} legs");
+                    Console.ReadLine();
+                    Console.WriteLine("Fried ending (6/7)");
+                }
+                else if (oldlady == "B")
+                {
+                    Console.WriteLine("You run away crying like the baby you are.");
+                    Console.ReadLine();
+                    Console.WriteLine("Cry baby ending(7/7)");
+                }
+                else if (oldlady == "Victory")
+                {
+                    Console.WriteLine("You activate you secret chicken powers and use your not so sexy chickenbeam. The corrosive chicken melts Old lady person and you stand victorious and is now known as The wierd chicken guy that nobody wants to be close to");
+                    Console.ReadLine();
+                    Console.WriteLine("Victory? ending (secret/7)");
+                }
+                else
+                {
+                    Console.WriteLine("Old lady person turns out to be ripped and super fast and smacks the shit out of you before you can react.");
+                    Console.ReadLine();
+                    Console.WriteLine("Sonic the Old laady(FAKE Ending(6/6))");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You just give up and die");
+                Console.ReadLine();
+                Console.WriteLine("I ran out of ideas(FAKE Ending(4/6))");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Since you couldn't decide what to do you started googling on how to escape Old lady persons basement when you name is {name} but all yu find is a link that says *10 reasons why {name} is cringe.");
+            Console.ReadLine();
+            Console.WriteLine("You end up dying because it's to much for you to handle.");
+            Console.ReadLine();
+            Console.WriteLine("Emotional damage(FAKE Ending(2/6))");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Sooooooo...there are good news and bad news.");
+        Console.ReadLine();
+        Console.WriteLine("Good news: you didn't get eaten by something");
+        Console.ReadLine();
+        Console.WriteLine("Bad news...Rocks shaped like The Rock started falling from the sky and one of them crushed you so you died.");
+        Console.ReadLine();
+        Console.WriteLine("The Rock(FAKE Ending (1/6))");
+    }
+}
+
+Console.ReadLine();
